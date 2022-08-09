3061484b3748870d800aa7f8097a2b798a6aa688
CarlosleonR <110928886+CarlosleonR@users.noreply.github.com>
2022-08-09 14:42:02 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'computer science'
+copyright = '2022, Carlos Leon'
+author = 'Carlos leon'
 
 
 
@@ -49,8 +49,8 @@ with open ('info.yml','r') as f:
 
 
 html_theme_options = {
-  "github_url": "https://github.com/username/",
-  "twitter_url": "https://twitter.com/username",
+  "github_url": "https://github.com/CarlosleonR/",
+  ",
   "search_bar_text": "Search this site...",
   "navbar_end": ["search-field.html"],
   "left_sidebar_end":[ "icon-links.html"]
